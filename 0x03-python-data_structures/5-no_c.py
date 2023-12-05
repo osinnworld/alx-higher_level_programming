bcb706b6e6bc89df91df7adcf9cd0d99d0a36134
osinnworld <shemogata@gmail.com>
2023-12-05 11:30:13 -0800

    second deadline

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = ''.join(char for in my_string if char not in 'Cc')
-    return my_string
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
