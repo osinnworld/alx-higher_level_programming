76bf184fa8e2e37889295fa40da3532a1ccfb2f2
osinnworld <shemogata@gmail.com>
2023-12-06 09:15:33 -0800

    1-2-3

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=None):
-    if matrix is None:
-        matrix = []
-
-    new_matrix = [[] for _ in range(len(matrix))]
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            squared_value = matrix[i][j] * matrix[i][j]
-            new_matrix[i].append(squared_value)
-        return new_matrix
+def square_matrix_simple(matrix=[]):
+    new_matrix = [[x ** 2 for x in row] for row in matrix]
+    return new_matrix
