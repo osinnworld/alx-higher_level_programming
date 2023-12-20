cb2507e5f190c2ec81b9b8637d70a99cad4402fd
osinnworld <shemogata@gmail.com>
2023-12-20 13:05:28 -0800

    0x06-python-classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-""" 
-Module contains empty class Square
-"""
+"""Define a class Square."""
 
 
 class Square:
-    """
-    This is an empty class.
-    """
+    """Represent a square."""
     pass
