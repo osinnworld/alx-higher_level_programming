a4069eaf3490a4287bb9c45c684633a94b21676e
osinnworld <shemogata@gmail.com>
2024-03-12 12:38:53 -0700

    advanced

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-// a script that imports an array and computes a new array
-const list = require('./100-data').list;
-const newList = list.map((element, index) => element * index);
+const list = require('./100-data.js').list;
 console.log(list);
-console.log(newList);
+console.log(list.map((item, index) => item * index));
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-// a class Square that defines a square and
-// inherits from class Rectangle
-const newSquare = require('./5-square');
-
-class Square extends newSquare {
-	charPrint (c = 'X') {
-		for (let i = 0; i < this.height; i++) {
-			console.log(c.repeat(this.height));
-		}
-	}
-}
-module.exports = Square;
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    }
+  }
+};
