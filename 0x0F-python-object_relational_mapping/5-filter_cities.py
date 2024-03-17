dcb0483dfde67173a532149582b0b33c1d6b95e2
osinnworld <shemogata@gmail.com>
2024-03-17 07:44:07 -0700

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,20 +1,36 @@
 #!/usr/bin/python3
-
-
+"""
+a script that lists all cities from the database hbtn_0e_4_usa
+"""
 import MySQLdb
-from sys import argv
-
+import sys
 
 if __name__ == "__main__":
-    database = MySQLdb.connect(user=argv[1],
-                               passwd=argv[2],
-                               db=argv[3])
-    curs = database.cursor()
-    curs.execute("SELECT cities.name FROM cities INNER JOIN states ON \
-                  cities.state_id=states.id WHERE states.name LIKE %s \
-                  ORDER BY cities.id ASC", [argv[4]])
-    rows = curs.fetchall()
-    s = []
-    print(", ".join(row[0] for row in rows))
-    curs.close()
-    database.close()
+    try:
+        db = MySQLdb.connect(
+                host='localhost',
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3],
+                port=3306
+                )
+        cur = db.cursor()
+        cur.execute("SELECT * FROM cities WHERE cities.state_id = "
+                    "(SELECT id FROM states WHERE name = "
+                    "\'{}\')".format(sys.argv[4]))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row[2], end='')
+            if row != rows[len(rows) - 1]:
+                print(', ', end='')
+        print()
+    except MySQLdb.Error as e:
+        try:
+            print("MySQL Error: [%d] - [%s]" % (e.args[0], e.args[1]))
+        except IndexError:
+            print("MySQL Error: [%s]" % str(e))
+    finally:
+        if 'cur' in locals() and cur is not None:
+            cur.close()
+        if 'db' in locals() and db is not None:
+            db.close()
