ba54d9ac49b334e4c770ae8cc0c440579f5d9968
osinnworld <shemogata@gmail.com>
2024-03-17 07:49:20 -0700

    7-model_state_fetch.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
-
-
-import sqlalchemy as db
+"""
+a script that lists all State objects
+from the database hbtn_0e_6_usa
+"""
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 from sys import argv
-
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = db.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-
-    Base.metadata.create_all(engine)
-
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    host = "localhost"
+    connection = f'mysql+mysqldb://{username}:\
+                {password}@{host}:3306/{database}'
+    engine = create_engine(connection)
     Session = sessionmaker(bind=engine)
-    session = Session()
+    session_1 = Session()
 
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
-
-    session.close()
+    states = session_1.query(State).order_by((State.id).asc()).all()
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
