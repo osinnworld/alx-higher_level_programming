6f981611140fecb801cc2e5254aad8ec09300098
osinnworld <shemogata@gmail.com>
2024-03-17 07:40:48 -0700

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python3
-
-
+"""
+a script that takes in arguments and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
+But this time, write one that is safe from MySQL injections!
+"""
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    database = MySQLdb.connect(user=argv[1],
-                               passwd=argv[2],
-                               db=argv[3])
-    curs = database.cursor()
-    curs.execute("SELECT * FROM states WHERE name\
-                  LIKE %s ORDER BY states.id\
-                  ASC", [argv[4]])
-    rows = curs.fetchall()
-    for row in rows:
-        print(row)
-    curs.close()
-    database.close()
+    try:
+        db = MySQLdb.connect(
+                host='localhost',
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3],
+                port=3306
+                )
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE name = %s"
+                    "ORDER BY states.id", (sys.argv[4],))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    except MySQLdb.Error as e:
+        try:
+            print("MySQL Error: [%d] - [%s]" % (e.args[0], e.args[1]))
+        except IndexError:
+            print("MySQL Error: [%s]" % str(e))
+    finally:
+        if 'cur' in locals() and cur is not None:
+            cur.close()
+        if 'db' in locals() and db is not None:
+            db.close()
