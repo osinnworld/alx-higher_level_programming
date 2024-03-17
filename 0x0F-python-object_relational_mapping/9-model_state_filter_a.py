28cd040f25b606d35a5c32622bdf8ec65d19c0a8
osinnworld <shemogata@gmail.com>
2024-03-17 07:52:30 -0700

    9-model_state_filter.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-
-
-import sqlalchemy as db
+"""
+a script that lists all State objects that contain the
+letter a from the database hbtn_0e_6_usa
+"""
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 from sys import argv
+from model_state import Base, State
 
 
 if __name__ == "__main__":
-    engine = db.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-
-    Base.metadata.create_all(engine)
-
+    username = argv[1]
+    passwd = argv[2]
+    host = 'localhost'
+    db = argv[3]
+    connection = f'mysql+mysqldb://{username}:{passwd}@{host}:3306/{db}'
+    engine = create_engine(connection)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).filter(
-            State.name.contains('a')).order_by(State.id.asc()):
-        print("{}: {}".format(state.id, state.name))
-
-    session.close()
+    states = session.query(State).filter(State.name.contains('a')).\
+        order_by((State.id).asc())
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
