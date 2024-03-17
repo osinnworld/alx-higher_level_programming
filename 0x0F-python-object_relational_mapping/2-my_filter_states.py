e4726a58824ab9deed04e8423694f7fc786e6ed9
osinnworld <shemogata@gmail.com>
2024-03-17 08:03:25 -0700

    2-my_filter_state.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python3
 """
-This script lists all states with
-a `name` starting with the letter `N`
-from the database `hbtn_0e_0_usa`.
+a script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument
 """
-
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
-    """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
-
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states \
-                 WHERE name LIKE BINARY 'N%' \
-                 ORDER BY states.id ASC")
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
+if __name__ == "__main__":
+    try:
+        db = MySQLdb.connect(
+                host='localhost',
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3],
+                port=3306
+                )
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE name=\'{}\'"
+                    "ORDER BY states.id".format(sys.argv[4]))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    except MySQLdb.Error as e:
+        try:
+            print("MySQL Error: [%d] - [%s]" % (e.args[0], e.args[1]))
+        except IndexError:
+            print("MySQL Error: [%s]" % str(e))
+    finally:
+        if 'cur' in locals() and cur is not None:
+            cur.close()
+        if 'db' in locals() and db is not None:
+            db.close()
